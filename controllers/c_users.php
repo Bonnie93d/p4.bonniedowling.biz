58736df6343e2221879d0b81b38b1dbfd680efd2
Bonnie Dowling <bonniedowling@comcast.net>
2013-12-22 21:33:47 -0500

    updating Github after clone issues

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -40,33 +40,6 @@ class users_controller extends base_controller {
     	$errors = NULL;
     	$passblank = NULL;
     	
-
-
-
-
-		#Check if email address entered exists in database
-		#SQL query for ID# associated with email entered
-		$q = "SELECT user_id
-	    FROM users
-	    WHERE email = '".$_POST['email']."'
-	    ";
-	
-		$test_email = DB::instance(DB_NAME)->select_field($q);
-		
-    	# set email test variable to T/F
-    	if ($test_email >0){
-	    	$new_email = NULL;
-    	}
-    	else {
-	    	$new_email = TRUE;
-    	} 
-    	
-    	if ($new_email == NULL){
-	    	//GO SOMEWHERE, PRODUCE ERROR MESSAGE
-	    	$email_error = "**Email already registered, please login to existing account**";
-	    	$errors = TRUE;
-    	}	    	
-
     	# remove uneeded fields and cleaning input data
 		unset($_POST['password2']);
 		unset($_POST['register']);
@@ -87,34 +60,12 @@ class users_controller extends base_controller {
 		    
 		# Insert this user into the database 
 		$user_id = DB::instance(DB_NAME)->insert("users", $_POST);
-		    
-		    
-		//$q = "SELECT user_id
-		//FROM users
-		//WHERE email = '".$_POST['email']."'
-		//";
-			
-		//$id = DB::instance(DB_NAME)->select_field($q);
-			
-		//unset($_POST);
-		//$_POST['created']  = Time::now();
-		//$_POST['user_id'] = $id;
-		//$_POST['user_id_followed'] = $id;
-		    
-		//$follow = DB::instance(DB_NAME)->insert("users_users", $_POST);
-			
-			
-		# Send them to the main page - or whever you want them to go
-		//Router::redirect("/");
-		//echo "all fields valid";
-		//echo 'You\'re signed up';
+		
 		Router::redirect("/users/login");
 
-			}	
+	}	
     		   
-    		      
-    
-
+    		     
 
  #LOGIN  ____________________________________________________ 
  
@@ -218,7 +169,7 @@ class users_controller extends base_controller {
             setcookie("token", $token, strtotime('+1 year'), '/');
 
             # Send them to the main page - or whever you want them to go
-            Router::redirect("/");
+            Router::redirect("/items/add");
 
 			}	
 			
@@ -276,261 +227,7 @@ class users_controller extends base_controller {
 
 				echo $emailTest;
 			}
-		}
-    	
-    				
-
-		
-		#PROFILE UPDATE ____________________________________________________ 
- 
-		public function profile($error = NULL) {
-	
-	        //If user is blank, they are not logged in. Redirect them to the login page
-	        if (!$this->user){
-	            Router::redirect('/users/login');
-	        }
-	
-	        //If they were not redirected away, continue:
-	
-	        //Set up the View
-	        $this->template->content = View::instance('v_users_profile');
-	        $this->template->title = "Profile of ".$this->user->first_name . " " . $this->user->last_name; 
-	        $this->template->content->user_name = $user_name;
-	
-	        // pass errors, if any
-	        $this->template->content->error = $error;
-	        
-	        //Display the view
-	        echo $this->template;
-	
-	    }
-	    
-	    public function p_profile() {
-    	#testing data entered
-    	$errors = NULL;
-    	$passblank = NULL;
-    	
-    	#Check if first name is blank, if so produce error
-    	if(empty($_POST['first_name'])){
-	    	$first_error = "**First name is required**";
-	    	$errors = TRUE;
-    	}
-    	else{
-	    	$first_error = "";
-    	}
-    	
-    	#Check if last  name is blank, if so produce error
-    	if(empty($_POST['last_name'])){
-	    	$last_error = "**Last name is required**";
-	    	$errors = TRUE;
-    	}
-    	else{
-	    	$last_error = "";
-    	}
-    	
-    	#Check if email is blank, if so produce error
-    	if(empty($_POST['email'])){
-	    	$email_error = "**Email is required**";
-	    	$errors = TRUE;
-    	}
-    	else{
-    	
-    	#Check if email is valid, if so produce error
-	    	if(!filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL )){
-		    	$email_error = "**Invalid email format**";
-		    	$errors = TRUE;
-	    	}
-			else{
-	    		$email_error = "";
-				}
-			}
-    	
-    	#Check if password is blank, if so produce error
-    	if(empty($_POST['password'])){
-	    	$password_error = "";
-	    	$passblank = TRUE;
-	    	$up_pw = NULL; 
-    	}
-    	else{
-	    	$password_error = "";
-	    	$pass1 = $_POST['password'];
-    	}
-    	
-    	#Check if confirm password is blank, if so produce error
-    	if(empty($_POST['password2'])){
-	    	$password2_error = "";
-	    	$passblank = TRUE;
-	    	$up_pw = NULL; 
-    	}
-    	else{
-	    	$password_error = "";
-	    	$pass2 = $_POST['password2'];
-    	}
-    	
-    	#Check if Password and confirm password match, if so continue, if not error with blank pw fields
-		if ($passblank == NULL){
-			if($pass1 === $pass2){
-				$password_error = "";
-				$pass1 = $_POST['password'];
-				$pass2 = $_POST['password2'];
-				$up_pw =  sha1(PASSWORD_SALT.$pass1); 
-			}
-			else{
-				$errors = TRUE;
-				$password_error = "**Passwords don't match, try again**";
-				$pass1 = "";
-				$pass2 = "";
-			}
-			
-		}  
-		
-		#Check if email address entered exists in database
-		#SQL query for ID# associated with email entered
-		$q = "SELECT user_id
-	    FROM users
-	    WHERE email = '".$_POST['email']."' && user_id != '".$_POST['user_id']."'
-	    ";
-	    
-	    	
-		$test_email = DB::instance(DB_NAME)->select_field($q);
-		
-    	# set email test variable to T/F
-    	if ($test_email >0){
-	    	$new_email = NULL;
-    	}
-    	else {
-	    	$new_email = TRUE;
-    	} 
-    	
-    	if ($new_email == NULL){
-	    	$email_error = "**Email already registered, please login to existing account**";
-	    	$errors = TRUE;
-    	}	    	
-
-    	## Relaod page with errors and entered data already in form fields
-    	if ($errors == TRUE){
-	    	$this->template->content = View::instance('v_users_profile');
-	        $this->template->title = "Profile of ".$this->user->first_name . " " . $this->user->last_name; 
-	        $this->template->content->user_name = $user_name;
-		    $this ->template -> client_files_body = "";
-		    $this ->template -> content ->first_error = $first_error;
-		    $this ->template -> content ->last_error = $last_error;
-		    $this ->template -> content ->password_error = $password_error;
-		    $this ->template -> content ->password2_error = $password2_error;
-		    $this ->template -> content ->email_error = $email_error;
-		    echo $this ->template;
-    	
-    	}
-		else{
-		
-			# cleaning input data
-
-			$_POST = DB::instance(DB_NAME)->sanitize($_POST);
-			
-			# More data we want stored with the user
-
-		    $modified = Time::now();
-		    		    
-			# Encrypt the password  
-		    $up_pw = sha1(PASSWORD_SALT.$up_pw);            
-		
-		    
-		    if ($up_pw ==  NULL){
-
-		    $qq = "UPDATE users 
-						SET first_name = '".$_POST['first_name']."',
-						last_name = '".$_POST['last_name']."',
-						email = '".$_POST['email']."',
-						modified = '".$modified."'
-						
-						WHERE user_id = '".$_POST['user_id']."'";	
-			$update = DB::instance(DB_NAME)->query($qq); 
-			}
-			else{
-					    $qq = 
-					    	
-			$update = DB::instance(DB_NAME)->query($qq); 
-				
-			}
-
-			$up_status = "Update Complete! ";
-			$this->template->content = View::instance('v_users_profile');
-	        $this->template->title = "Profile of ".$this->user->first_name . " " . $this->user->last_name; 
-	        $this->template->content->user_name = $user_name;
-		    $this ->template -> content ->up_status = $up_status;
-		    echo $this ->template;
-
-		    
-		    
-		    
-		    # Insert this user into the database 
-			# Send them to the main page - or whever you want them to go
-
-		 
-
-			}	
-			
 	}
-    
-	    public function im_profile() {
-        
-		//Сheck for file
-
-		if((!empty($_FILES["uploaded_file"]))) {
-			//Check the file is JPG 
-		  $filename = basename($_FILES['uploaded_file']['name']);
-		  $filename = strtolower($filename);
-		  $ext = substr($filename, strrpos($filename, '.') + 1);
-		  
-		  $filename = $_POST['i_name'].".jpg";
-		  
-		  if (($ext == "jpg") && ($_FILES["uploaded_file"]["type"] == "image/jpeg") &&
-			    ($_FILES["uploaded_file"]["size"] < 10485760)){
-			    
-				    #folder the photos will be saved in
-				      $save_location = APP_PATH.'/libraries/profile_images/'.$filename;
-				      
-				      #Move the photo
-				    if ((move_uploaded_file($_FILES['uploaded_file']['tmp_name'],$save_location))) {
-				      
-						$img = imagecreatefromjpeg($save_location);
-						$wid = imagesx($img);
-						$hght = imagesy($img);
-						
-						#resize
-						$x = imagecreatetruecolor(200, 200);
-						
-						imagecopyresampled($x, $img, 0, 0, 0, 0, 200, 200, $wid, $hght);
-						
-						#update saved file
-						imagejpeg($x, $save_location);  
-						
-						#update DB with filename
-						$qq = "UPDATE users 
-						SET profile_image = '".$filename."' 
-						WHERE email = '".$_POST['email']."'";	
-						$update = DB::instance(DB_NAME)->query($qq); 
-
-
-				     $up_status = "Upload Complete! ";
-
-		        } 
-		       		  } 
-		       else {
-		       $up_status = "Error! Please submit a valid JPEG file. ";
-
-			   }
-		} 
-		
-		#return to Profile page with message of success or failure
-			$this->template->content = View::instance('v_users_profile');
-	        $this->template->title = "Profile of ".$this->user->first_name . " " . $this->user->last_name; 
-	        $this->template->content->user_name = $user_name;
-		    $this ->template -> content ->image_error = $up_status;
-		    echo $this ->template;
-
-			}
-			
-	   
+    	 				
 
 } # end of the class
